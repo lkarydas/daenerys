d4c3b70bd731eb724b635e68004ce4904c5fed8a
Laz <lkarydas@gmail.com>
2013-08-29 18:48:07 -0400

    Added simple test case for reversing single linked list.

--- src/CodePracticeMain.java
+++ src/CodePracticeMain.java
@@ -58,6 +58,10 @@ public class CodePracticeMain {
     
     list.print();
     
+    ListProblems listProblems = new ListProblems();
+    list = listProblems.reverseIteratively(list);
+    list.print();
+    
   }
 
 }
