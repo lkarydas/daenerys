460c904f778af15ea209367a2377d72cdafe834e
Laz <lkarydas@gmail.com>
2013-08-29 18:59:25 -0400

    Added explanatory comments.

--- src/ListProblems.java
+++ src/ListProblems.java
@@ -1,6 +1,11 @@
 
 public class ListProblems {
 
+	/**
+	 * Iteratively reverse a single linked list.
+	 * @param list The head of the list.
+	 * @return the new head of the list.
+	 */
 	ListElement<Integer> reverseIteratively(ListElement<Integer> list) {
 		ListElement<Integer> currentElement, nextElement, previousElement;
 		previousElement = null;
