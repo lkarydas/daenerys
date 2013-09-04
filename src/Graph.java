46ef32263c22213d115c111de25d47e10124cbae
Laz <lkarydas@gmail.com>
2013-09-04 13:35:37 -0400

    Methods to build a graph by adding edges.

--- src/Graph.java
+++ src/Graph.java
@@ -1,9 +1,53 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 
 class Graph {
-	ArrayList<GraphEdge> edges;
-	int nvertices;
-	int nedges;
-	
+	HashMap<Integer, GraphEdge> adj;
+
+	/**
+	 * Empty graph constructor.
+	 */
+	Graph() {
+		adj = new HashMap<Integer, GraphEdge>();
+	}
+
+	void addEdge(int sourceNode, int targetNode, int weight) {
+		addDirectedEdge(sourceNode, targetNode, weight);
+		addDirectedEdge(targetNode, sourceNode, weight);
+	}
+
+	void addDirectedEdge(int sourceNode, int targetNode, int weight) {
+		if (adj.containsKey(sourceNode)) {
+			// Find the last edge in the linked list.
+			GraphEdge edge = adj.get(sourceNode);
+			while (edge.hasNext()) {
+				edge = edge.getNext();
+			}
+			edge.setNext(new GraphEdge(targetNode, weight));
+		} else {
+			adj.put(sourceNode, new GraphEdge(targetNode, weight));
+		}
+	}
+
+	int getVertexCount() {
+		return adj.size();
+	}
+
+	void print() {
+		for (int i = 0; i < getVertexCount(); i++) {
+			System.out.print("Node " + i + ": ");
+			if (adj.get(i) == null) {
+				System.err.println("<Unconnected Node!>");
+			} else {
+				GraphEdge edge = adj.get(i);
+				System.out.print(edge.toString() + " ");
+				while (edge.hasNext()) {
+					edge = edge.getNext();
+					System.out.print(edge.toString() + " ");
+				}
+				System.out.println("");
+			}
+		}
+	}
+
 }
--- src/GraphEdge.java
+++ src/GraphEdge.java
@@ -1,6 +1,45 @@
 
 public class GraphEdge {
-	int y;
-	int weight;
-	GraphEdge next;
+	private int y;
+	private int weight;
+	private GraphEdge next;
+	
+	public GraphEdge(int targetNode, int weight) {
+		y = targetNode;
+		this.weight = weight;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public boolean hasNext() {
+		return next != null;
+	}
+	
+	public GraphEdge getNext() {
+		return next;
+	}
+
+	public void setNext(GraphEdge next) {
+		this.next = next;
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + y + ", " + weight + ")"; 
+	}
+	
 }
